299e0ccfda0e3e717604b714da3931cc2b829f8b
gangadharkondappagari <127930435+gangadharkondappagari@users.noreply.github.com>
2023-07-24 15:23:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-red-500 underline text-center">
+      Hello world!
+    </h1>
+  )
+}
